0848bb797b4db21297c8fcd5deb540b9b8aef109
abdasalaaam <79949406+abdasalaaam@users.noreply.github.com>
2021-03-07 11:49:47 -0800

    Update demo.rkt

--- demo.rkt
+++ demo.rkt
@@ -23,117 +23,6 @@
       ((null? tree) state)
       ((number? state) state)
       (else (evaluate-line (car tree) (cdr tree) (M_state line state))))))
-
-;name gets the variable name in a declaration/assignment statement
-(define name cadr)
-
-;expression gets the expression in the assignment statement
-(define expression caddr)
-
-;condition gets the condition in a if or while statement
-(define condition cadr)
-
-;then-statement gets the then statement in a if or while statement
-(define then-statement caddr)
-
-;return-expression gets the return expression in a return statement
-(define return-expression cadr)
-
-;line-type gets the type of the line, the first element in the line
-(define line-type car)
-
-;next-value gets the next values in a list
-(define next-value cdr)
-
-;curr-value gets the current value in a list
-(define curr-value car)
-
-;adds a variable and its value to state, if the value has been declared, but not assigned, its corresponding value is null
-(define Add_M_state
-  (lambda (name value state)
-    (list (cons name (car state)) (cons value (cadr state)))))
-
-;removes a variable and its corresponding value from the state by calling the remove function
-(define Remove_M_state
-  (lambda (name state)
-    (remove name (car state) (cadr state) '() '())))
-
-;helper function to remove a variable from state list, if variable isn't found then the original state is returned from a saved list
-(define remove
-  (lambda (name declare-list value-list saved-declare saved-value)
-    (cond
-      ((null? declare-list) (list saved-declare saved-value))
-      ((eq? name (curr-value declare-list)) (list (append saved-declare (next-value declare-list)) (append saved-value (next-value value-list))))
-      (else (remove name (next-value declare-list) (next-value value-list) (cons (curr-value declare-list) saved-declare) (cons (curr-value value-list) saved-value))))))
-
-
-;reterns the state of an expression by calling on its respective function, otherwise the current state will be returned
-(define M_state
-  (lambda (expression state)
-    (cond
-      ((null? expression) state)
-      ((not (list? expression)) state)
-      ((list? (line-type expression)) (M_state (cdr expression) (M_state (car expression) state)))
-      ((eq? (line-type expression) 'return) (return-expression (cadr expression) state))
-      ((eq? (line-type expression) 'var) (declaration (name expression) expression state))
-      ((eq? (line-type expression) '=) (assignment (name expression) (caddr expression) state))
-      ((eq? (line-type expression) 'if) (if-statement (cadr expression) (caddr expression) expression state))
-      ((eq? (line-type expression) 'while) (while-statement (cadr expression) (caddr expression) state))
-      (else state))))
-
-
-;returns the boolean result of boolean and comparison operations 
-;each operand will be sent to M_value for interpretation 
-(define M_boolean
-  (lambda (expression state)
-    (cond
-      ((not (list? expression)) (M_value expression state))
-      ((eq? (operator expression) '&&) (and (M_boolean (leftoperand expression) state) (M_boolean (rightoperand expression) state)))
-      ((eq? (operator expression) '||) (or (M_boolean (leftoperand expression) state) (M_boolean (rightoperand expression) state)))
-      ((eq? (operator expression) '!) (not (M_boolean (leftoperand expression) state)))
-      ((eq? (operator expression) '==) (= (M_value (leftoperand expression) state) (M_value (rightoperand expression) state)))
-      ((eq? (operator expression) '!=) (not (= (M_value (leftoperand expression) state) (M_value (rightoperand expression) state))))
-      ((eq? (operator expression) '<) (< (M_value (leftoperand expression) state) (M_value (rightoperand expression) state)))
-      ((eq? (operator expression) '<=) (<= (M_value (leftoperand expression) state) (M_value (rightoperand expression) state)))
-      ((eq? (operator expression) '>) (> (M_value (leftoperand expression) state) (M_value (rightoperand expression) state)))
-      ((eq? (operator expression) '>=) (>= (M_value (leftoperand expression) state) (M_value (rightoperand expression) state)))
-      (else (error "Invalid")))))
-      
-;returns the value of expression, whether it is a boolean, variable, arithmetic expression, or number
-(define M_value
-  (lambda (expression state)
-    (cond
-      ((number? expression) expression)
-      ((eq? expression 'true) #t)
-      ((eq? expression 'false) #f)
-      ((not (pair? expression)) (get_from_state expression (car state) (cadr state)))
-      ((eq? (operator expression) '+) (+ (M_value (leftoperand expression) state) (M_value (rightoperand expression) state)))
-      ((eq? (operator expression) '/) (quotient (M_value (leftoperand expression) state) (M_value (rightoperand expression) state)))
-      ((eq? (operator expression) '%) (remainder (M_value (leftoperand expression) state) (M_value (rightoperand expression) state)))
-      ((and (eq? (operator expression) '-) (null? (cddr expression))) (- (M_value (leftoperand expression) state))) 
-      ((eq? (operator expression) '-) (- (M_value (leftoperand expression) state) (M_value (rightoperand expression) state)))
-      ((eq? (operator expression) '*) (* (M_value (leftoperand expression) state) (M_value (rightoperand expression) state)))
-      ((eq? (operator expression) 'var) (M_value (leftoperand expression) (M_state expression state)))
-      ((eq? (operator expression) '=) (M_value (leftoperand expression) (M_state expression state)))
-      (else (M_boolean expression state))))) ;if the value of expression is either a boolean test (like >) or if the expression is invalid
-      
-;helper function for finding the operator of an expression
-(define operator car)
-
-;helper function for finding the left operand of an expression
-(define leftoperand cadr) 
-
-;helper function for finding the right operand of an expression
-(define rightoperand caddr)
-
-;reads through the declared/value list bindings and returns the value of the variable if found in declare-list and if it is assigned
-(define get_from_state
-  (lambda (name declare-list value-list)
-    (cond
-      ((null? declare-list) (error "Not declared"))
-      ((and (eq? name (car declare-list)) (eq? (car value-list) 'null)) (error "Not Assigned")) ;if the binding for variable name is null it is not assigned
-      ((and (eq? name (car declare-list))) (car value-list))                                    ;returns value of name if it has been found
-      (else (get_from_state name (cdr declare-list) (cdr value-list))))))
       
 ;if value of name is a non number/not a boolean, its undeclared
 ;line - the entire declaration expression
@@ -168,6 +57,15 @@
       ((null? declare-list) error "Not Declared")
       ((eq? (car declare-list) name) (not (eq? (car value-list) 'null))) ;if the name is found in the declare-list and the value is not null, return true
       (else (is_assigned name (cdr declare-list) (cdr value-list))))))
+      
+;reads through the declared/value list bindings and returns the value of the variable if found in declare-list and if it is assigned
+(define get_from_state
+  (lambda (name declare-list value-list)
+    (cond
+      ((null? declare-list) (error "Not declared"))
+      ((and (eq? name (car declare-list)) (eq? (car value-list) 'null)) (error "Not Assigned")) ;if the binding for variable name is null it is not assigned
+      ((and (eq? name (car declare-list))) (car value-list))                                    ;returns value of name if it has been found
+      (else (get_from_state name (cdr declare-list) (cdr value-list))))))
 
 ;returns the value of expression using the state and M_value function
 (define return
@@ -189,4 +87,103 @@
     (if (M_boolean condition (M_state condition state))
         (while-statement condition body-statement (M_state body-statement (M_state condition state))) ;if condtion is true, run while statement again on the changed state
         (M_state condition state))))
+        
+;gets the condition in a if or while statement
+(define get-condition cadr)
 
+;gets the then statement in a if or while statement
+(define get-then-statement caddr)
+
+;adds a variable and its value to state, if the value has been declared, but not assigned, its corresponding value is null
+(define Add_M_state
+  (lambda (name value state)
+    (list (cons name (car state)) (cons value (cadr state)))))
+
+;removes a variable and its corresponding value from the state by calling the remove function
+(define Remove_M_state
+  (lambda (name state)
+    (remove name (car state) (cadr state) '() '())))
+
+;helper function to remove a variable from state list, if variable isn't found then the original state is returned from a saved list
+(define remove
+  (lambda (name declare-list value-list saved-declare saved-value)
+    (cond
+      ((null? declare-list) (list saved-declare saved-value))
+      ((eq? name (curr-value declare-list)) (list (append saved-declare (next-value declare-list)) (append saved-value (next-value value-list))))
+      (else (remove name (next-value declare-list) (next-value value-list) (cons (curr-value declare-list) saved-declare) (cons (curr-value value-list) saved-value))))))
+
+;curr-value gets the current value in a list
+(define curr-value car)
+
+;next-value gets the next values in a list
+(define next-value cdr)
+
+;reterns the state of an expression by calling on its respective function, otherwise the current state will be returned
+(define M_state
+  (lambda (expression state)
+    (cond
+      ((null? expression) state)
+      ((not (list? expression)) state)
+      ((list? (line-type expression)) (M_state (cdr expression) (M_state (car expression) state)))
+      ((eq? (line-type expression) 'return) (return-expression (cadr expression) state))
+      ((eq? (line-type expression) 'var) (declaration (get-name expression) expression state))
+      ((eq? (line-type expression) '=) (assignment (get-name expression) (caddr expression) state))
+      ((eq? (line-type expression) 'if) (if-statement (cadr expression) (caddr expression) expression state))
+      ((eq? (line-type expression) 'while) (while-statement (cadr expression) (caddr expression) state))
+      (else state))))
+      
+;return-expression gets the return expression in a return statement
+(define return-expression cadr)
+
+;get-expression gets the expression in the assignment statement
+(define get-expression caddr)
+
+;get-name gets the variable name in a declaration/assignment statement
+(define get-name cadr)
+
+;line-type gets the type of the line, the first element in the line
+(define line-type car)
+
+;returns the boolean result of boolean and comparison operations 
+;each operand will be sent to M_value for interpretation 
+(define M_boolean
+  (lambda (expression state)
+    (cond
+      ((not (list? expression)) (M_value expression state))
+      ((eq? (operator expression) '&&) (and (M_boolean (leftoperand expression) state) (M_boolean (rightoperand expression) state)))
+      ((eq? (operator expression) '||) (or (M_boolean (leftoperand expression) state) (M_boolean (rightoperand expression) state)))
+      ((eq? (operator expression) '!) (not (M_boolean (leftoperand expression) state)))
+      ((eq? (operator expression) '==) (= (M_value (leftoperand expression) state) (M_value (rightoperand expression) state)))
+      ((eq? (operator expression) '!=) (not (= (M_value (leftoperand expression) state) (M_value (rightoperand expression) state))))
+      ((eq? (operator expression) '<) (< (M_value (leftoperand expression) state) (M_value (rightoperand expression) state)))
+      ((eq? (operator expression) '<=) (<= (M_value (leftoperand expression) state) (M_value (rightoperand expression) state)))
+      ((eq? (operator expression) '>) (> (M_value (leftoperand expression) state) (M_value (rightoperand expression) state)))
+      ((eq? (operator expression) '>=) (>= (M_value (leftoperand expression) state) (M_value (rightoperand expression) state)))
+      (else (error "Invalid")))))
+
+;gets the left operand of an expression
+(define leftoperand cadr)
+
+;gets the right operand of an expression
+(define rightoperand caddr)
+
+;operator gets the operator of an expression
+(define operator car)
+
+;returns the value of expression, whether it is a boolean, variable, arithmetic expression, or number
+(define M_value
+  (lambda (expression state)
+    (cond
+      ((number? expression) expression)
+      ((eq? expression 'true) #t)
+      ((eq? expression 'false) #f)
+      ((not (pair? expression)) (get_from_state expression (car state) (cadr state)))
+      ((eq? (operator expression) '+) (+ (M_value (leftoperand expression) state) (M_value (rightoperand expression) state)))
+      ((eq? (operator expression) '/) (quotient (M_value (leftoperand expression) state) (M_value (rightoperand expression) state)))
+      ((eq? (operator expression) '%) (remainder (M_value (leftoperand expression) state) (M_value (rightoperand expression) state)))
+      ((and (eq? (operator expression) '-) (null? (cddr expression))) (- (M_value (leftoperand expression) state))) 
+      ((eq? (operator expression) '-) (- (M_value (leftoperand expression) state) (M_value (rightoperand expression) state)))
+      ((eq? (operator expression) '*) (* (M_value (leftoperand expression) state) (M_value (rightoperand expression) state)))
+      ((eq? (operator expression) 'var) (M_value (leftoperand expression) (M_state expression state)))
+      ((eq? (operator expression) '=) (M_value (leftoperand expression) (M_state expression state)))
+      (else (M_boolean expression state))))) ;if the value of expression is either a boolean test (like >=) or if the expression is invalid
